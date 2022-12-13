1694583f2d79c39f835c886cd2c9dac82b5872f4
karrmao <karrmao@gmail.com>
2022-12-13 19:45:47 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/header/Header.jsx';
 import Calendar from './components/calendar/Calendar.jsx';
+import Modal from './components/modal/Modal';
 
 import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
 
 import './common.scss';
 
-class App extends Component {
-  state = {
-    weekStartDate: new Date(),
+const App = () => {
+  const [weekStartDate] = useState(new Date());
+  const [eventForm, isOpenModalForm] = useState(false);
+
+  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+  //console.log(weekDates);
+
+  const showEventForm = () => {
+    isOpenModalForm(true);
   };
 
-  render() {
-    const { weekStartDate } = this.state;
-    console.log(weekStartDate);
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
-    console.log(weekDates);
+  const closeEventForm = () => {
+    isOpenModalForm(false);
+  };
 
-    return (
-      <>
-        <Header />
-        <Calendar weekDates={weekDates} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Header showEventForm={showEventForm} />
+      <Calendar weekDates={weekDates} />
+      {eventForm && <Modal closeEventForm={closeEventForm} />}
+    </>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     weekStartDate: new Date(),
+//   };
+
+//   render() {
+//     const { weekStartDate } = this.state;
+//     console.log(weekStartDate);
+//     const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+//     console.log(weekDates);
+
+//     return (
+//       <>
+//         <Header />
+//         <Calendar weekDates={weekDates} />
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
